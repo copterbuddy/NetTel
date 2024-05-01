3a05f84185ae9ddffdd6b0afbea431517b9bf780
copter <copterbuddy@gmail.com>
2024-05-01 16:45:29 +0700

    add Serilog to console

--- NetTel/Program.cs
+++ NetTel/Program.cs
@@ -1,8 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .WriteTo.OpenTelemetry()
+    .CreateLogger();
+
+builder.Host.UseSerilog(Log.Logger);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -19,7 +29,7 @@ builder.Services.AddOpenTelemetry()
         .AddProcessInstrumentation()
         .AddOtlpExporter(opt =>
         {
-            opt.Endpoint = new Uri(builder.Configuration["Otpl:MetricEndpoint"]);
+            opt.Endpoint = new Uri(builder.Configuration["Otpl:MetricEndpoint"] ?? throw new Exception());
         })
     )
     .WithTracing(opt =>
@@ -29,7 +39,7 @@ builder.Services.AddOpenTelemetry()
         .AddHttpClientInstrumentation()
         .AddJaegerExporter(opt =>
         {
-            opt.Endpoint = new Uri(builder.Configuration["Otpl:TracingEndpoint"]);
+            opt.Endpoint = new Uri(builder.Configuration["Otpl:TracingEndpoint"] ?? throw new Exception());
         })
 
     );
@@ -50,8 +60,9 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/weatherforecast", (Serilog.ILogger logger) =>
 {
+    logger.Warning("this is my log");
     var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
